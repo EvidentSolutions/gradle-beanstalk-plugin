ea3602ffaabb8d0b16820efb0bb2ad5e0393cd2c
Nick Spacek <nick.spacek@xiplinx.com>
2017-02-25 12:40:01 -0400

    Adds ability to specify prefix for the version string for EB

---
+++ src/test/java/fi/evident/gradle/beanstalk/DeployTaskTest.java
@@ -0,0 +1,31 @@
+package fi.evident.gradle.beanstalk;
+
+import java.util.Map;
+import java.util.HashMap;
+import org.junit.Test;
+import org.gradle.api.Project;
+import org.gradle.testfixtures.ProjectBuilder;
+
+import static org.junit.Assert.assertTrue;
+
+public class DeployTaskTest {
+    @Test
+    public void versionShouldIncludePrefix() {
+        Project project = ProjectBuilder.builder().build();
+        project.setVersion("version");
+        Map<String, Object> params = new HashMap<>();
+        params.put("type", TestDeployTask.class);
+        TestDeployTask task = (TestDeployTask) project.task(params, "tasktest");
+        BeanstalkDeployment deployment = new BeanstalkDeployment("tasktest");
+        deployment.setVersionPrefix("test-");
+        task.setDeployment(deployment);
+
+        assertTrue(task.findVersionLabel().matches("test-version"));
+    }
+
+    public static class TestDeployTask extends DeployTask {
+        private String findVersionLabel() {
+            return getVersionLabel();
+        }
+    }
+}
