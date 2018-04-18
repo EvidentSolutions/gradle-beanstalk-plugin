dd10ebb0994cc254c33dd7a908fa520eb81b4474
Ovadia-B <ovadia.b@gmail.com>
2018-04-18 08:00:40 +0300

    Add Gradle task for each deployment
    Instead of using a rule to create tasks dynamically as needed, create tasks
    during initialization so that they show up in task listings.

--- src/main/java/fi/evident/gradle/beanstalk/BeanstalkPlugin.java
+++ src/main/java/fi/evident/gradle/beanstalk/BeanstalkPlugin.java
@@ -6,12 +6,22 @@ import org.gradle.api.Project;
 import org.gradle.api.plugins.ExtensionAware;
 
 public class BeanstalkPlugin implements Plugin<Project> {
-    public void apply(final Project project) {
+    public void apply(Project project) {
         NamedDomainObjectContainer<BeanstalkDeployment> deployments = project.container(BeanstalkDeployment.class);
         BeanstalkPluginExtension beanstalk = project.getExtensions().create("beanstalk", BeanstalkPluginExtension.class);
-
         ((ExtensionAware) beanstalk).getExtensions().add("deployments", deployments);
 
-        project.getTasks().addRule(new DeployRule(deployments, project, beanstalk));
+        project.afterEvaluate(p -> {
+            deployments.forEach(deployment -> {
+                        String name = deployment.getName();
+                        String task = "deploy" + name.substring(0, 1).toUpperCase() + name.substring(1);
+
+                        p.getTasks().create(task, DeployTask.class, deployTask -> {
+                            deployTask.setBeanstalk(beanstalk);
+                            deployTask.setDeployment(deployment);
+                        });
+                    }
+            );
+        });
     }
 }
